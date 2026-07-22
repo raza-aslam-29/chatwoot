847ef770c99b9d49bbebe9787480e78f1364aca4
Muhammad Raza <raza.aslam23385@gmail.com>
2026-07-22 20:46:23 +0500

    Secure cookie store for staging and production envs

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -3,4 +3,4 @@
 Rails.application.config.session_store :cookie_store,
                                        key: '_channelx_session',
                                        same_site: :lax,
-                                       secure: Rails.env.production?
+                                       secure: !Rails.env.local?
